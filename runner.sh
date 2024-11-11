04c4caa635f9d0c59b94778151cbc2bf3e3f9707
Dieg0407 <diego.pastor.0407@gmail.com>
2024-11-10 19:53:48 -0500

    chore: fixing the runner

--- runner.sh
+++ runner.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 last_week=$(date -d "last week" +"%Y-%m-%d")
-log_path="/var/log/scrubber/$last_week.log"
+log_path="/var/log/scrubber/$last_week.execution.log"
 report_path="/var/custom/scrubber/reportes-$last_week.csv"
 
-/opt/custom/scrubber -d $last_week > reportes-$last_week.csv 2> $last_week.execution.log
+/opt/custom/scrubber -d $last_week > $report_path 2> $log_path
