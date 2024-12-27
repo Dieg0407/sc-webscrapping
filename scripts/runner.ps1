f5db669a43dce72889025fe58168ec0bad5f6cba
Dieg0407 <diego.pastor.0407@gmail.com>
2024-12-26 21:23:35 -0500

    chore: fixed windows runner

--- scripts/runner.ps1
+++ scripts/runner.ps1
@@ -1,21 +1,9 @@
-$execution_date = $args[0]
+$execution_date = Read-Host "Ingresa la fecha a procesar (yyyy-MM-dd): "
 
-# Check if date is empty
-if ([string]::IsNullOrEmpty($execution_date)) {
-  $execution_date = (Get-Date).AddDays(-7).ToString("yyyy-MM-dd")
+if (!(Test-Path -Path "reportes")) {
+  New-Item -ItemType Directory -Path "reportes"
 }
 
-# Check if "logs" directory exists, create it if not
-if (!(Test-Path -Path "logs")) {
-  New-Item -ItemType Directory -Path "logs"
-}
+$report_path = "reportes\reporte-$execution_date.csv"
 
-# Check if "reports" directory exists, create it if not
-if (!(Test-Path -Path "reports")) {
-  New-Item -ItemType Directory -Path "reports"
-}
-
-$log_path = "logs\$execution_date.execution.log"
-$report_path = "reports\reportes-$execution_date.csv"
-
-.\scrubber.exe -d $execution_date | Out-File -FilePath $report_path -Encoding utf8 2>&1 | Out-File -FilePath $log_path -Encoding utf8
+.\scrubber.exe -d $execution_date > $report_path
